d13e7967e11b227c2bec2cc6e289f6e0c917d006
Aleksandr Kalashnik <heyas@yandex.ru>
2018-01-03 17:17:53 +0300

    Add even game

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { games } from '..';
+import { even } from '../index';
 
 console.log('Welcome to the Brain Games!');
-games();
+even();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export const greetings = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  return name;
+};
+
+export const even = () => {
+  const ask = (name) => {
+    const questionNumber = Math.floor(Math.random() * 1000);
+    console.log('Question:', questionNumber);
+    const userAnswer = readlineSync.question('May I have your name? ');
+    const rightAnswer = questionNumber % 2 === 0 ? 'yes' : 'no';
+    if (userAnswer === rightAnswer) {
+      console.log('Correct!');
+      return true;
+    }
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${name}!`);
+    return false;
+  };
+
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  const name = greetings();
+  for (let rightAnswers = 0; rightAnswers < 3;) {
+    if (ask(name)) {
+      rightAnswers += 1;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
 };
