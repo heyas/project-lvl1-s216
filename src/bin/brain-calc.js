7f82584401f739607da2c0dc16293ce3696fce3e
Aleksandr Kalashnik <heyas@yandex.ru>
2018-01-04 23:46:59 +0300

    Add calc game

---
+++ src/bin/brain-calc.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { calc } from '..';
+
+console.log('Welcome to the Brain Games!');
+calc();
--- src/index.js
+++ src/index.js
@@ -6,29 +6,58 @@ export const greetings = () => {
   return name;
 };
 
-export const even = () => {
-  const ask = (name, userAnswer) => {
+const ask = () => readlineSync.question();
 
-  };
+const generateQuestion = () => {
+  const question = Math.floor(Math.random() * 1000);
+  const rightAnswer = question % 2 === 0 ? 'yes' : 'no';
+  return [question, rightAnswer];
+};
 
-  console.log('Answer "yes" if number even otherwise answer "no".');
+const generateCalc = () => {
+  const number1 = Math.floor(Math.random() * 10);
+  const number2 = Math.floor(Math.random() * 10);
+  const operandArray = [
+    {
+      func: (a, b) => a - b,
+      symbol: '-',
+    },
+    {
+      func: (a, b) => a + b,
+      symbol: '+',
+    },
+    {
+      func: (a, b) => a * b,
+      symbol: '*',
+    },
+  ];
+  const operand = operandArray[Math.floor(Math.random() * operandArray.length)];
+  const rightAnswer = operand.func(number1, number2);
+  return [number1 + operand.symbol + number2, rightAnswer];
+};
+
+export const createGame = questionGenerateFunction => () => {
   const name = greetings();
   for (let rightAnswers = 0; rightAnswers < 3;) {
-    const questionNumber = Math.floor(Math.random() * 1000);
-    console.log('Question:', questionNumber);
-    const userAnswer = readlineSync.question('Question:');
-    const rightAnswer = questionNumber % 2 === 0 ? 'yes' : 'no';
-    if (userAnswer === rightAnswer) {
-      console.log('Correct!');
+    const [question, rightAnswer] = questionGenerateFunction();
+    console.log('Question:', question);
+    const userAnswer = ask();
+    if (userAnswer == rightAnswer) {
       rightAnswers += 1;
+      console.log('Correct!');
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${name}!`);
-      return false;
+      return;
     }
   }
   console.log(`Congratulations, ${name}!`);
 };
 
+export const even = createGame(generateQuestion);
+export const calc = createGame(generateCalc);
+
 export const games = () => {
   greetings();
+  const question = Math.floor(Math.random() * 1000);
+  console.log('Question:', question);
 };
