2e51193ac272e748efe6b9ea026783cf70108d77
Aleksandr Kalashnik <heyas@yandex.ru>
2018-01-04 17:21:58 +0300

    Change game-even loogic

--- src/index.js
+++ src/index.js
@@ -7,24 +7,23 @@ export const greetings = () => {
 };
 
 export const even = () => {
-  const ask = (name) => {
-    const questionNumber = Math.floor(Math.random() * 1000);
-    console.log('Question:', questionNumber);
-    const userAnswer = readlineSync.question('May I have your name? ');
-    const rightAnswer = questionNumber % 2 === 0 ? 'yes' : 'no';
-    if (userAnswer === rightAnswer) {
-      console.log('Correct!');
-      return true;
-    }
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${name}!`);
-    return false;
+  const ask = (name, userAnswer) => {
+
   };
 
   console.log('Answer "yes" if number even otherwise answer "no".');
   const name = greetings();
   for (let rightAnswers = 0; rightAnswers < 3;) {
-    if (ask(name)) {
+    const questionNumber = Math.floor(Math.random() * 1000);
+    console.log('Question:', questionNumber);
+    const userAnswer = readlineSync.question('Question:');
+    const rightAnswer = questionNumber % 2 === 0 ? 'yes' : 'no';
+    if (userAnswer === rightAnswer) {
+      console.log('Correct!');
       rightAnswers += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${name}!`);
+      return false;
     }
   }
   console.log(`Congratulations, ${name}!`);
