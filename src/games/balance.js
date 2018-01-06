ebf86abafe0dab0ae4623163d1d181dd9a7dad2d
Aleksandr Kalashnik <heyas@yandex.ru>
2018-01-06 15:17:45 +0300

    Fix eslint errors

--- src/games/balance.js
+++ src/games/balance.js
@@ -1,15 +1,15 @@
 import game from '..';
 import random from '../utils';
 
-const main = () => {    
+const main = () => {
   const balance = (str) => {
     const isBalanced = num => Math.max(...num) - Math.min(...num) <= 1;
 
     const arr = [...str].sort((a, b) => a - b);
 
     while (!isBalanced(arr)) {
-      arr[0] = parseInt(arr[0]) + 1;
-      arr[arr.length - 1] = parseInt(arr[arr.length - 1]) - 1;
+      arr[0] = parseInt(arr[0], 10) + 1;
+      arr[arr.length - 1] = parseInt(arr[arr.length - 1], 10) - 1;
       arr.sort((a, b) => a - b);
     }
     return arr.join('');
