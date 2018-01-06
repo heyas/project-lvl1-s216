9792c712793d421d732750f013ba38981eec1a80
Aleksandr Kalashnik <heyas@yandex.ru>
2018-01-06 23:33:02 +0300

    Make a progression game over recursion

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,19 +2,20 @@ import game from '..';
 import random from '../utils';
 
 const main = () => {
-  const randomNumber = random(1, 10);
-  const action = num => num + randomNumber;
-  let num = random(1, 10);
-  const row = [];
-  for (let i = 0; i < 10; i += 1) {
-    row.push(num);
-    num = action(num);
-  }
+  const step = random(1, 10);
+  const questionElement = random(1, 10);
+  const startValue = random(1, 100);
+  const progression = (n, iteration, question, rightAnswer) => {
+    if (iteration === 0) {
+      return [question, rightAnswer];
+    }
+    if (iteration === questionElement) {
+      return progression(n + step, iteration - 1, `${question}...`, n);
+    }
+    return progression(n + step, iteration - 1, `${question}${n},`, rightAnswer);
+  };
 
-  const rightAnswer = row[5];
-  row[5] = '...';
-  const question = row.join();
-  return [question, rightAnswer];
+  return progression(startValue, 10, '');
 };
 
 export default game(main);
